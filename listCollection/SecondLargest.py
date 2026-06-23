820a237ecda317e9108a0efb9c716c25bbc3c849
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-06-23 21:22:16 +0530

    Implement second largest number finder
    Added logic to find the second largest number without using built-in functions.

--- listCollection/SecondLargest.py
+++ listCollection/SecondLargest.py
@@ -4,3 +4,19 @@ numbers.sort()
 second_largest = numbers[-2]
 
 print("Second Largest:", second_largest)
+
+#without inbuilt fun
+
+numbers = [10, 20, 4, 45, 99]
+
+largest = numbers[0]
+second_largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print("Second Largest:", second_largest)
