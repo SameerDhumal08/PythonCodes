043f4e87ad7ac2669827a62bae7d733d9b26f4c0
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-06-24 23:37:39 +0530

    Update commonElements.py

--- set_Collection/commonElements.py
+++ set_Collection/commonElements.py
@@ -3,3 +3,15 @@ b = [3, 4, 5, 6]
 
 common = set(a) & set(b)
 print(common)
+
+#without set
+a = [1, 2, 3, 4]
+b = [3, 4, 5, 6]
+
+common = []
+
+for i in a:
+    if i in b:
+        common.append(i)
+
+print(common)
