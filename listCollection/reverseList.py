834150373bd0fb9965766fdcdb9c1d10724bdafa
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-06-24 23:29:19 +0530

    Enhance reverseList.py with multiple reverse methods
    Added examples for reversing a list in Python using different methods.

--- listCollection/reverseList.py
+++ listCollection/reverseList.py
@@ -2,3 +2,28 @@ numbers = [1, 2, 3, 4, 5]
 
 numbers.reverse()
 print(numbers)
+
+#with new list
+
+numbers = [1, 2, 3, 4, 5]
+
+reversed_list = []
+
+for i in range(len(numbers) - 1, -1, -1):
+    reversed_list.append(numbers[i])
+
+print(reversed_list)
+
+#without new list
+
+numbers = [1, 2, 3, 4, 5]
+
+start = 0
+end = len(numbers) - 1
+
+while start < end:
+    numbers[start], numbers[end] = numbers[end], numbers[start]
+    start += 1
+    end -= 1
+
+print(numbers)
