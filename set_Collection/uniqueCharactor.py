6ef385273deddd34de891496331eac959e2ca313
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-06-25 19:56:18 +0530

    Implement unique character count without set
    Added a method to count unique characters without using a set.

--- set_Collection/uniqueCharactor.py
+++ set_Collection/uniqueCharactor.py
@@ -2,3 +2,14 @@ text = "hello world"
 unique_chars = set(text)
 
 print(len(unique_chars))
+
+##without set
+text = "hello world"
+
+unique_chars = []
+
+for char in text:
+    if char not in unique_chars:
+        unique_chars.append(char)
+
+print(len(unique_chars))
