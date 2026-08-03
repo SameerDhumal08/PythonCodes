d469e56e3d1ae76115312e94ed15fc83846af0a8
Sameer Dhumal <106860465+SameerDhumal08@users.noreply.github.com>
2026-08-03 21:29:45 +0530

    Implement basic queue operations in QusingList.py

---
+++ QusingList.py
@@ -0,0 +1,14 @@
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue:", queue)
+
+# Dequeue
+removed = queue.pop(0)
+print("Removed:", removed)
+
+print("Queue after dequeue:", queue)
